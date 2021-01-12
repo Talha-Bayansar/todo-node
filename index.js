68841a99b91b41aa369fd87093ccb1947ed91750
Talha Bayansar <talha.bayansar@hotmail.com>
2021-01-12 23:43:43 +0100

    added gitignore

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const {data} = require('./data/data');
 const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = 3000;
+const conString = 'mongodb+srv://talha:bayansar@cluster0.hgfnb.mongodb.net/nodetodo?retryWrites=true&w=majority';
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+MongoClient.connect(conString, {
+    useUnifiedTopology: true
+  }, (error, client) => {
+if (error) return console.log(error)
+console.log('Connected to db!')
+})
 
 app.get('/', (req, res) => {
     res.json(data);
